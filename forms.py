4bd9923a76f0ac011ebc9e3945e5a2fc394d30b6
Agnieszka <agnieszka.ab.babicz@gmail.com>
2025-11-06 19:15:47 +0100

    Review all TODOs.

--- forms.py
+++ forms.py
@@ -27,7 +27,7 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("LET ME IN!")
 
-# TODO: Create a CommentForm so users can leave comments below posts
+# Create a CommentForm so users can leave comments below posts
 class CommentForm(FlaskForm):
     comment_text = CKEditorField('Comment', validators=[DataRequired()])
     submit = SubmitField('SUBMIT COMMENT')
