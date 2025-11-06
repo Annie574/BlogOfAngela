2fef8e1d78c3d1e1016400ff832aaacd4091658a
Agnieszka <agnieszka.ab.babicz@gmail.com>
2025-11-06 19:14:13 +0100

    Use env variables and create Procfile.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from typing import List
 from flask import Flask, abort, render_template, redirect, url_for, flash
@@ -14,7 +15,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -35,7 +36,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
